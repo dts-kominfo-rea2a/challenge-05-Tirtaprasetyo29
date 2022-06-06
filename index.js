509be98b267885deaa93c5056403c441d75299de
Tirtaprasetyo29 <tirtaprasetyo111@gmail.com>
2022-06-06 21:25:25 +0700

    revisi 3 tugas 5

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = (listNama, callBack) => {
+/* const sorter = (listNama, callBack) => {
   let simpanArray = callBack(listNama)
 
   let hasilArray =[]
@@ -14,7 +14,21 @@ const sorter = (listNama, callBack) => {
       hasilArray.push(i+1 + '.' + simpanArray[i])
     }
   return hasilArray;
-}
+} */
+const sorter = (arrName, cbLogicSorter) => {
+  // create container array
+  let arrStringOutput = [];
+
+  // loop to fill the container array
+  for(counter = 0; counter < arrName.length; counter++) {
+    // fill the container array with callback logic sorter.
+    arrStringOutput.push(`${counter+1}. ${cbLogicSorter(arrName)[counter]}`)
+  }
+
+  // return a sorter array of string
+  return arrStringOutput;
+};
+
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
